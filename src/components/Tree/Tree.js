f0992cbc5d6a0678722fe9840d24027d975204be
azerbelhedi <azerbelhedide@gmail.com>
2019-05-05 01:16:10 +0100

    FileBox support web files

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -9,19 +9,56 @@ class Tree extends Component{
         this.state = {
             tree : []
         }
+        this.fileLogo = this.fileLogo.bind(this) 
     }
 
     fileLogo(fileName){
         // return { ex , color , name}
+        let arr = fileName.split(".") 
+        let ex = arr[1]
+        ex = ex.toUpperCase() ;
+        let color ;
+        switch(ex){
+            case "JS" :
+                color = "yellow"
+            break
+
+            case "HTML" :
+                color = "orange"
+            break
+            
+            case "CSS" :
+                color = "#1ca8ff"
+            break   
+
+            case "JSON" :
+                color = "red"
+            break 
+
+            case "MD" :
+                color = "#4460ff"
+                ex = "INFO"
+            break
+
+            default :
+                color = "white"
+                ex = "FILE"
+            break
+        }
+        return(
+            {name : fileName , ex : ex , color : color}
+        )
     }
 
     render(){
         return(
             <div className="tree">
-                <FileBox data = {{name : "main.js" , ex : "JS" , color : "yellow"}} />
-                <FileBox data = {{name : "index.html" , ex : "HTML" , color : "orange"}} />
-                <FileBox data = {{name : "style.css" , ex : "CSS" , color : "#1ca8ff"}} />
-                <FileBox data = {{name : "style.idk" , ex : "FILE" , color : "white"}} />
+                <FileBox data = {this.fileLogo("main.js")} />
+                <FileBox data = {this.fileLogo("index.html")} />
+                <FileBox data = {this.fileLogo("style.css")} />
+                <FileBox data = {this.fileLogo("config.json")} />
+                <FileBox data = {this.fileLogo("azer.idk")} />
+                <FileBox data = {this.fileLogo("readme.md")} />
 
                 {this.props.data.tree.map(file => {
                     console.log(file)
